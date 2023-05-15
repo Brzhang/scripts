0d3d848c70f79c5b7db6f00348848e3b9336f4b7
Brook <zhangjt@weigutech.com>
2023-05-15 15:48:08 +0800

    bilibili video download support play list

--- bilibiliVideoDown.py
+++ bilibiliVideoDown.py
@@ -1,51 +1,81 @@
 # *** author:Brook zhang
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
+# use: python bilibiliVideoDown.py url [count]
 import os
 import sys
 import re
 import random
 import requests
 import json
-
-path = './'
+from tqdm import tqdm
 
 class BilibiliDownloader():
-    def __init__(self, s_url):
+    def __init__(self, s_url, dst_path):
         self.url = s_url
+        self.dst_path = dst_path
         self.header = {
             'Range': 'bytes=0-',
             'referer': self.url,
             'origin': 'https://www.bilibili.com/',
-            # 'cookie':'',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.63'
+            'cookie':''
         }
 
     def Bl_download(self):
         html = requests.get(self.url, headers=self.header).text
         json_data = re.findall('window.__playinfo__=(.*?)</script>', html)[0]
-        video_name=re.findall(',"title":"(.*?)","', html)[0]
+        video_name = re.findall('<title.*?>(.*?)</title>', html)[0]
         if video_name == '':
             video_name = int(random.random() * 2 * 1000)
-        if len(str(video_name)) > 20:
-            video_name = video_name[:20]
+        '''if len(str(video_name)) > 20:
+            video_name = video_name[:20]'''
+        video_name = re.sub('[\s]', '', video_name)
+        video_name = re.sub('^.', '', video_name)
+        video_name = re.sub('/', '', video_name)
         video = json.loads(json_data)['data']['dash']['video'][0]['baseUrl']
-        self.download(video,path+video_name+'.flv')
+        self.download(video, self.dst_path+video_name+'.flv')
         print("[bilibili]: {} video download completed.".format(video_name))
         audio = json.loads(json_data)['data']['dash']['audio'][0]['baseUrl']
-        self.download(audio, path + video_name + '.mp3')
+        self.download(audio, self.dst_path + video_name + '.mp3')
         print("[bilibili]: {} audio download completed.".format(video_name))
 
 
     def download(self,url,rel_path):
-        r = requests.get(url, headers=self.header)
+        r = requests.get(url, headers=self.header, stream=True)
+        file_size = int(r.headers['content-length'])
+        
+        if os.path.exists(rel_path):
+            first_byte = os.path.getsize(rel_path)  # (3)
+        else:
+            first_byte = 0
+        if first_byte >= file_size: # (4)
+            return True
+
+        #header = {"Range": f"bytes={first_byte}-{file_size}"}
+        
+        size = 0
+    
+        pbar = tqdm(total=file_size, initial=0, unit='B', unit_scale=True, desc=rel_path)
         with open(rel_path, 'wb')as f:
-            f.write(r.content)
-            print("writing file......")
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    size += len(chunk)
+                    f.write(chunk)
+                    pbar.update(8192)
+        pbar.close()
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('You must enter the url')
         exit(1)
     video_url = sys.argv[1]
-    BilibiliDownloader(video_url).Bl_download()
+    if len(sys.argv) > 2:
+        video_count = int(sys.argv[2])
+    else:
+        video_count = 1
+    path = './'
+    if video_count > 1:
+        for i in range(video_count):
+            BilibiliDownloader(video_url+'?p='+str(i+1), path).Bl_download()
+    else:
+        BilibiliDownloader(video_url, path).Bl_download()
